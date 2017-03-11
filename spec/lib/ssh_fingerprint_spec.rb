02a7f96bab15ff511d4ba06c178759cf3662d810
Victor Gama <hey@vito.io>
2017-03-11 14:38:12 -0300

    Update tests

--- spec/lib/ssh_fingerprint_spec.rb
+++ spec/lib/ssh_fingerprint_spec.rb
@@ -1,22 +1,24 @@
 require 'ssh-fingerprint'
-require 'coveralls'
-Coveralls.wear!
+require 'simplecov'
+SimpleCov.start
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 describe 'Fingerprint' do
   let(:key) do
     File.read(File.expand_path('../../support/id_rsa.pub', __FILE__))
   end
-  
+
   # Obtained through ssh-keygen -lf ../support/id_rsa.pub
   let(:known_fingerprint) { 'f5:d8:39:1d:7c:26:0c:07:97:b2:d5:09:2f:dd:45:e4' }
-  
+
   it 'should not break the key apart!' do
     current_key = key.clone
     used_key = key.clone
     SSHFingerprint.compute(used_key)
     used_key.should eq(current_key)
   end
-  
+
   it 'should match known fingerprint' do
     SSHFingerprint.compute(key).should eq(known_fingerprint)
   end
--- ssh-fingerprint.gemspec
+++ ssh-fingerprint.gemspec
@@ -13,14 +13,14 @@ Gem::Specification.new do |spec|
   spec.summary        = %q{SSH fingerprint generator}
   spec.homepage       = 'http://github.com/victorgama/ssh-fingerprint'
   spec.license        = 'MIT'
-  
+
   spec.files          = `git ls-files`.split($/)
   spec.executables    = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files     = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths  = ["lib"]
-  
+
   spec.add_development_dependency 'bundler',    '~> 1.5'
   spec.add_development_dependency 'rake',       '~> 10.1'
   spec.add_development_dependency 'rspec',      '~> 2.14'
-  spec.add_development_dependency 'coveralls',  '~> 0.7'
+  spec.add_development_dependency 'codecov'
 end
